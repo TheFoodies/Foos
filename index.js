e7846193f6d015a8ad70eeabc9cbfbcb7ce99a58
Daniel Soffiantini <Daniel Soffiantini>
2016-07-15 12:12:12 -0600

    added CRUD endpoints for restaurant and cart

--- index.js
+++ index.js
@@ -36,7 +36,48 @@ app.use(express.static('public'));
 
 
 //Cart
+app.get('/api/cart/:id', function(req, res, next) {
+  Cart.findById(req.params.id, function(err, cartResponse) {
+    if (err) {
+      console.log(err)
+    } else {
+      res.status(200).json(cartResponse)
+    }
+  })
+})
 
+app.post('/api/cart/', function(req, res, next) {
+  Cart.create(function(err, cartResponse) {
+    var newCart = new Cart(req.body);
+    newCart.save(function(err, saved) {
+      if(err) {
+        console.log(err)
+      } else {
+        res.status(200).json(saved)
+      }
+    })
+  })
+})
+
+app.put('/api/cart/:id', function(req, res, next) {
+  Cart.findByIdAndUpdate(req.params.id, req.body, function(err, cartResponse) {
+    if(err) {
+      console.log(err)
+    } else {
+      res.status(200).json(cartResponse)
+    }
+  })
+})
+
+app.delete('/api/cart/:id', function(req, res, next) {
+  Cart.findByIdAndRemove(req.params.id, function(err, cartResponse) {
+    if (err) {
+      console.log(err)
+    } else {
+      res.status(200).json(cartResponse)
+    }
+  })
+})
 
 //Food
 app.get('/api/food/:id', function(req, res, next) {
@@ -86,7 +127,48 @@ app.delete('/api/food/:id', function(req, res, next) {
 
 
 //Restaurant
+app.get('/api/restaurant/:id', function(req, res, next) {
+  Restaurant.findById(req.params.id, function(err, restaurantResponse) {
+    if (err) {
+      console.log(err)
+    } else {
+      res.status(200).json(restaurantResponse)
+    }
+  })
+})
 
+app.post('/api/restaurant/', function(req, res, next) {
+  Restaurant.create(function(err, restaurantResponse) {
+    var newRestaurant = new Restaurant(req.body);
+    newRestaurant.save(function(err, saved) {
+      if(err) {
+        console.log(err)
+      } else {
+        res.status(200).json(saved)
+      }
+    })
+  })
+})
+
+app.put('/api/restaurant/:id', function(req, res, next) {
+  Restaurant.findByIdAndUpdate(req.params.id, req.body, function(err, restaurantResponse) {
+    if(err) {
+      console.log(err)
+    } else {
+      res.status(200).json(restaurantResponse)
+    }
+  })
+})
+
+app.delete('/api/restaurant/:id', function(req, res, next) {
+  Restaurant.findByIdAndRemove(req.params.id, function(err, restaurantResponse) {
+    if (err) {
+      console.log(err)
+    } else {
+      res.status(200).json(restaurantResponse)
+    }
+  })
+})
 
 //User
 
