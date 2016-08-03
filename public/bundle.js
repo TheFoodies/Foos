b2004e6695f3ae9ab4bb3ee8ad5e131294e48e7e
Jordan Groff <joman.groff@gmail.com>
2016-08-03 16:13:48 -0600

    fixed merge conflicts

--- public/bundle.js
+++ public/bundle.js
@@ -660,6 +660,13 @@ angular.module("foodie").service("yelpService", ["$q", "$http", function ($q, $h
 }
 /******/]);
 //# sourceMappingURL=angular-modal-service.js.map
+angular.module('foodie').directive('navbar', function () {
+  return {
+    restrict: 'EA',
+    templateUrl: './app/directives/navbar/navbar.html',
+    controller: 'homeController'
+  };
+});
 angular.module("foodie").controller("loginModalController", ["$scope", "close", function ($scope, close) {
 
   $scope.close = close;
@@ -694,13 +701,6 @@ angular.module("foodie").controller("loginModalController", ["$scope", "close", 
     }
   };
 }]);
-angular.module('foodie').directive('navbar', function () {
-  return {
-    restrict: 'EA',
-    templateUrl: './app/directives/navbar/navbar.html',
-    controller: 'homeController'
-  };
-});
 angular.module("foodie").controller("cartController", ["$scope", "$state", "$stateParams", "orderService", "cartService", "restaurantService", "user", function ($scope, $state, $stateParams, orderService, cartService, restaurantService, user) {
 
   // $scope.cart = {
@@ -1004,6 +1004,88 @@ angular.module('foodie').controller('homeController', ["$scope", "userService", 
     });
   };
 }]);
+angular.module("foodie").controller("menuController", ["$scope", "ngDialog", "yelpService", "cartService", "restaurantService", "$stateParams", "userService", "$state", "user", function ($scope, ngDialog, yelpService, cartService, restaurantService, $stateParams, userService, $state, user) {
+
+    $scope.user = user;
+
+    $scope.getRestaurant = function () {
+        restaurantService.getRestaurantInfo($stateParams.restaurantID).then(function (restaurant) {
+            $scope.restaurant = restaurant;
+            $scope.menu = restaurant.menu;
+        });
+    };
+
+    $scope.getRestaurant();
+
+    $scope.findAveragePrice = function () {
+        var sum = 0;
+        var items = 0;
+        var average = 0;
+        for (var i = 0; i < menu.length; i++) {
+
+            for (var j = 0; j < menu[i].items.length; j++) {
+                sum += menu[i].items[j].price;
+                items += 1;
+            }
+        }
+        average = sum / items;
+        if (average > 0 && average <= 10) {
+            $scope.averagePrice = "$";
+        } else if (average > 10 && average <= 20) {
+            $scope.averagePrice = "$$";
+        } else if (average > 20 && average <= 30) {
+            $scope.averagePrice = "$$$";
+        } else {
+            $scope.averagePrice = "$$$$";
+        }
+    };
+
+    $scope.getCart = function () {
+        cartService.getCart($stateParams.restaurantID, user._id).then(function (cart) {
+            console.log('this is the getCart', cart);
+            $scope.cart = cart.items;
+        });
+    };
+
+    $scope.getCart();
+
+    $scope.addToCart = function (item, quantity, specialInstructions) {
+        var itemsObj = new Object();
+        itemsObj.item = item;
+        itemsObj.quantity = quantity;
+        itemsObj.specialInstructions = specialInstructions;
+        console.log(itemsObj);
+        cartService.addToCart(itemsObj, $stateParams.restaurantID, user._id).then(function (cart) {
+            console.log('here is the cart', cart.items);
+
+            ngDialog.close();
+            $scope.cart = cart.items;
+        });
+    };
+
+    $scope.restaurantImage = 'https://i.kinja-img.com/gawker-media/image/upload/wafswectpmbr0zmug9ly.jpg';
+
+    $scope.clickToOpen = function (item) {
+        var newScope = $scope.$new();
+        newScope.item = item;
+        ngDialog.open({
+            template: './app/routes/menu/item-modal.html',
+            scope: newScope
+        });
+    };
+
+    $scope.quantity = 1;
+
+    $scope.addQuantity = function () {
+        $scope.quantity++;
+    };
+
+    $scope.removeQuantity = function () {
+        if ($scope.quantity > 1) {
+            $scope.quantity--;
+        }
+    };
+}]);
 angular.module("foodie").controller("orderController", ["$scope", "$http", "orderService", function ($scope, $http, orderService) {
 
   // $scope.orderFeed = function(){
@@ -1123,4 +1205,18 @@ angular.module('foodie').controller('restaurantController', ["$scope", "restaura
             $scope.averagePrice = "$$$$";
         }
     };
+}]);
+angular.module("foodie").controller("successController", ["$scope", "$stateParams", "user", "orderService", function ($scope, $stateParams, user, orderService) {
+
+  $scope.getOneOrder = function () {
+    orderService.getOneOrder($stateParams.orderID).then(function (order) {
+      $scope.order = order.items;
+      $scope.baseOrder = order;
+      console.log("success order is ", order);
+    });
+  };
+
+  $scope.getOneOrder();
+
+  $scope.user = user;
 }]);
